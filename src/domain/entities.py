6e4ea58751e176ae5dc68202e572735fff49ce58
plat01 <fplatoff@gmail.com>
2024-12-16 21:30:23 +0400

    add user entity

---
+++ src/domain/entities.py
@@ -0,0 +1,15 @@
+from uuid import UUID
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    refresh_token: str
+    expires_in: int
+
+class User(BaseModel):
+
+    id: UUID | str
+    username: str
+    token : Token
+    
